d03b29b2f8b183704be76e887b39fe4537edbe57
Lans12345 <olanalans12345@gmail.com>
2022-11-20 16:40:39 +0800

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_school_management/screens/home_page.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
